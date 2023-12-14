798a558f20382fe443061650570af8b7f9367c10
santiago Álvarez <santiagoaas10@gmail.com>
2023-12-14 18:32:18 -0500

    clase 2 xmlhttprequest comentado

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,20 +1,33 @@
-const XMLHttpRequest = requiere('xmlhttprequest');
+// Requiere el módulo xmlhttprequest y asigna la clase XMLHttpRequest a la constante XMLHttpRequest
+const XMLHttpRequest = require('xmlhttprequest');
 
+// URL de la API a la que se realizarán las solicitudes
 const API = 'https://api.escuelajs.co/api/v1';
- 
-function fetchData(urlAPI, callback){
+
+// Función para realizar solicitudes HTTP
+function fetchData(urlAPI, callback) {
+    // Crea una nueva instancia de XMLHttpRequest
     let xhttp = new XMLHttpRequest();
 
-    xhttp.open('GET', urlApi, true);
-    xhttp.onreadystatechange= function(event) {
-        if(xhttp.readyState === 4){
-            if(xhttp.status===200){
-                callback(null, JSON.parse(xhttp.responseText))
+    // Configura la solicitud GET a la URL proporcionada de manera asíncrona
+    xhttp.open('GET', urlAPI, true);
+
+    // Establece un evento para manejar cambios en el estado de la solicitud
+    xhttp.onreadystatechange = function (event) {
+        // Verifica si la solicitud ha alcanzado el estado "completado"
+        if (xhttp.readyState === 4) {
+            // Verifica si el estado de la respuesta es "OK" (código 200)
+            if (xhttp.status === 200) {
+                // Llama al callback con nulo para el error y el objeto JSON parseado como resultado
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                // Si el estado de la respuesta no es "OK", crea un objeto Error y llama al callback con el error y nulo como resultado
+                const error = new Error('Error ' + urlAPI);
+                return callback(error, null);
             }
-        } else{
-            const error = new Error('Error' + urlApi);
-            return callback(error,null)
         }
-    }
+    };
+
+    // Envía la solicitud
     xhttp.send();
 }
